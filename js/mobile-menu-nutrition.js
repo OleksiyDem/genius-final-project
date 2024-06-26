4d68e751bb6ccbd1cdded1b35ec649ac86551b9c
Oleksiy <alexey60@gmail.com>
2024-06-26 20:57:00 +0300

    finish progect

--- js/mobile-menu-nutrition.js
+++ js/mobile-menu-nutrition.js
@@ -1,12 +1,4 @@
-const mobileMenu = document.querySelector('.mobile-menu');
 const mobileMenuNutrition = document.querySelector('.mobile-menu-nutrition');
-const menuBtnOpen = document.querySelector('.menu-btn-open');
-const menuBtnClose = document.querySelector('.menu-btn-close');
-const menuBtnClose1 = document.querySelector('.menu-btn-close1');
-const menuBtnClose2 = document.querySelector('.menu-btn-close2');
-const menuBtnClose3 = document.querySelector('.menu-btn-close3');
-const menuBtnClose4 = document.querySelector('.menu-btn-close4');
-
 const menuBtnOpenNutrition = document.querySelector('.menu-btn-open-nutrition');
 const menuBtnCloseNutrition = document.querySelector(
   '.menu-btn-close-nutrition',
@@ -24,25 +16,10 @@ const menuBtnClose4Nutrition = document.querySelector(
   '.menu-btn-close4-nutrition',
 );
 
-const toggleMenu = () => mobileMenu.classList.toggle('is-open');
 const toggleMenuNutrition = () =>
   mobileMenuNutrition.classList.toggle('is-open');
 const disableScroll = () =>
   document.body.classList.toggle('is-scroll-disabled');
-
-menuBtnOpen.addEventListener('click', toggleMenu);
-menuBtnClose.addEventListener('click', toggleMenu);
-menuBtnClose1.addEventListener('click', toggleMenu);
-menuBtnClose2.addEventListener('click', toggleMenu);
-menuBtnClose3.addEventListener('click', toggleMenu);
-menuBtnClose4.addEventListener('click', toggleMenu);
-
-menuBtnOpen.addEventListener('click', disableScroll);
-menuBtnClose.addEventListener('click', disableScroll);
-menuBtnClose1.addEventListener('click', disableScroll);
-menuBtnClose2.addEventListener('click', disableScroll);
-menuBtnClose3.addEventListener('click', disableScroll);
-menuBtnClose4.addEventListener('click', disableScroll);
 
 menuBtnOpenNutrition.addEventListener('click', toggleMenuNutrition);
 menuBtnCloseNutrition.addEventListener('click', toggleMenuNutrition);
--- js/slider.js
+++ js/slider.js
@@ -25,6 +25,7 @@ var swiper = new Swiper('.swiper-happycustomer', {
     el: '.pagination',
     bulletClass: 'pagination__button',
     bulletActiveClass: 'pagination__button--active',
+    clickable: true,
   },
 
   // Navigation arrows
@@ -40,6 +41,10 @@ var swiper = new Swiper('.swiper-services', {
 
   // Default parameters к-ть слайдів на екрані
   slidesPerView: 1,
+
+  // grid: {
+  //   rows: 2,
+  // },
 
   // відстань між слайдами, px
   spaceBetween: 20,
@@ -57,5 +62,63 @@ var swiper = new Swiper('.swiper-services', {
     1280: {
       slidesPerView: 4,
     },
+  },
+});
+
+var swiper = new Swiper('.swiper-nutrition', {
+  // Optional parameters
+  loop: true,
+
+  // Default parameters к-ть слайдів на екрані
+  slidesPerView: 2,
+  slidesPerGroup: 2,
+
+  // Параметры модуля Grid
+  grid: {
+    rows: 3, // Количество рядов в сетке
+    fill: 'row', // Заполнять ряды слева направо
+  },
+  // відстань між слайдами, px
+  spaceBetween: 16,
+
+  // Responsive breakpoints
+  breakpoints: {
+    // when window width is >= 768px
+    768: {
+      slidesPerView: 3,
+      slidesPerGroup: 3,
+      grid: {
+        rows: 2, // Количество рядов в сетке
+        fill: 'row', // Заполнять ряды слева направо
+      },
+      spaceBetween: 16,
+    },
+    // when window width is >= 1280px
+    1280: {
+      slidesPerView: 4,
+      slidesPerGroup: 4,
+      grid: {
+        rows: 2, // Количество рядов в сетке
+        fill: 'row', // Заполнять ряды слева направо
+      },
+      spaceBetween: 32,
+    },
+  },
+
+  // If we need pagination
+  pagination: {
+    el: '.pagination-nutrition',
+    bulletClass: 'pagination-nutrition__button',
+    bulletActiveClass: 'pagination-nutrition__button--active',
+    clickable: true,
+    renderBullet: function (index, className) {
+      return '<span class="' + className + '">' + (index + 1) + '</span>';
+    },
+  },
+
+  // Navigation arrows
+  navigation: {
+    nextEl: '.carousel-button-nutrition.next',
+    prevEl: '.carousel-button-nutrition.prev',
   },
 });
